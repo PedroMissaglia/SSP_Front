27b1a08fab3f855d2246e03fac5ab06e4edc66d0
PedroMissaglia <45774945+PedroMissaglia@users.noreply.github.com>
2019-11-07 13:17:53 -0300

    Remoção do compEstoque via route

--- routes/products.js
+++ routes/products.js
@@ -57,53 +57,6 @@ router.get('/', async function(req, res, next) {
     console.log('Erro');
     }
 
-    try {
-      const response = 
-      await axios.get( urlProtheusCompEst,
-        {headers: {'Authorization': basicAuth}}
-      );
-      Jsondata = response.data;
-    if (response.status == '200'){
-      compEstoque = Jsondata.products
-
-      console.log(compEstoque);
-      
-      var mydatasets = [];
-
-      for(var j = 0; j < compEstoque.length; j++) {
-        mydatasets.push(compEstoque[j].description)
-      }
-
-      var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
-      var donutData = {
-        labels: [
-            'Chrome', 
-            'IE',
-            'FireFox', 
-            'Safari', 
-            'Opera', 
-            'Navigator', 
-            ],
-        datasets: [
-          {
-            data: [700,500,400,600,300,100],
-            backgroundColor : ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
-          }]
-        }
-      var donutOptions     = {
-        maintainAspectRatio : false,
-        responsive : true,
-        }
-        var donutChart = new Chart(donutChartCanvas, {
-          type: 'doughnut',
-          data: donutData,
-          options: donutOptions      
-        })  
-    }
-    }catch (error) {
-    console.log('Erro');
-    }
-
     res.render('products', { title: 'Express', products: products, top3: top3, lotValidity: lotValidity });
   });
   
