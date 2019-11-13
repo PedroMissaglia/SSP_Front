1b0d5af50034de56f3906f80d9f2b1255ac3a23a
PedroMissaglia <45774945+PedroMissaglia@users.noreply.github.com>
2019-11-13 16:13:32 -0300

    Adição Vendas 2

--- routes/sales.js
+++ routes/sales.js
@@ -6,9 +6,11 @@ router.get('/', async function(req, res, next) {
 
     var address = 'http://localhost:1243/rest/teste';
     var urlProtheusprofitVenda = address + '/lucroVenda';
+    var urlProtheusTopClientesValor = address + '/clienteValor';
     var basicAuth = 'Basic YWRtaW46IA==';
     var Jsondata;
     let lucroVenda = [];
+    let clientesValor = [];
 
     try {
         const response = 
@@ -22,8 +24,23 @@ router.get('/', async function(req, res, next) {
     } catch (error) {
       console.log('Falha ao listar os produtos.');
     }
+
+    try {
+        const response = 
+        await axios.get( urlProtheusTopClientesValor,
+          {headers: {'Authorization': basicAuth}}
+        );
+        Jsondata = response.data;
+      if (response.status == '200'){
+        clientesValor = Jsondata.clients
+      }
+    } catch (error) {
+      console.log('Falha ao listar os produtos.');
+    }
+    
     res.render('sales', { title: 'Express',
-     lucroVenda: lucroVenda});
+     lucroVenda: lucroVenda,
+    clientesValor: clientesValor});
   });
 
 module.exports = router;
